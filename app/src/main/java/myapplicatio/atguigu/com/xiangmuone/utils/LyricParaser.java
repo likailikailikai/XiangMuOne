a4efe55255fdf301a887612b9fb8d7d861c4377a
likailikailikai <1360869846@qq.com>
2017-01-14 00:40:49 +0800

    解决歌词乱码问题

--- app/src/main/java/myapplicatio/atguigu/com/xiangmuone/utils/LyricParaser.java
+++ app/src/main/java/myapplicatio/atguigu/com/xiangmuone/utils/LyricParaser.java
@@ -1,5 +1,6 @@
 package myapplicatio.atguigu.com.xiangmuone.utils;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -46,7 +47,7 @@ public class LyricParaser {
             //解析歌词-一句一句
             BufferedReader reader = null;
             try {
-                reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "GBK"));
+                reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), getCharset(file)));
 
                 String line;
                 while ((line = reader.readLine()) != null) {
@@ -73,7 +74,74 @@ public class LyricParaser {
         }
     }
 
+    /**
+     * 判断文件编码
+     * @param file 文件
+     * @return 编码：GBK,UTF-8,UTF-16LE
+     */
+    public String getCharset(File file) {
+        String charset = "GBK";
+        byte[] first3Bytes = new byte[3];
+        try {
+            boolean checked = false;
+            BufferedInputStream bis = new BufferedInputStream(
+                    new FileInputStream(file));
+            bis.mark(0);
+            int read = bis.read(first3Bytes, 0, 3);
+            if (read == -1)
+                return charset;
+            if (first3Bytes[0] == (byte) 0xFF && first3Bytes[1] == (byte) 0xFE) {
+                charset = "UTF-16LE";
+                checked = true;
+            } else if (first3Bytes[0] == (byte) 0xFE
+                    && first3Bytes[1] == (byte) 0xFF) {
+                charset = "UTF-16BE";
+                checked = true;
+            } else if (first3Bytes[0] == (byte) 0xEF
+                    && first3Bytes[1] == (byte) 0xBB
+                    && first3Bytes[2] == (byte) 0xBF) {
+                charset = "UTF-8";
+                checked = true;
+            }
+            bis.reset();
+            if (!checked) {
+                int loc = 0;
+                while ((read = bis.read()) != -1) {
+                    loc++;
+                    if (read >= 0xF0)
+                        break;
+                    if (0x80 <= read && read <= 0xBF)
+                        break;
+                    if (0xC0 <= read && read <= 0xDF) {
+                        read = bis.read();
+                        if (0x80 <= read && read <= 0xBF)
+                            continue;
+                        else
+                            break;
+                    } else if (0xE0 <= read && read <= 0xEF) {
+                        read = bis.read();
+                        if (0x80 <= read && read <= 0xBF) {
+                            read = bis.read();
+                            if (0x80 <= read && read <= 0xBF) {
+                                charset = "UTF-8";
+                                break;
+                            } else
+                                break;
+                        } else
+                            break;
+                    }
+                }
+            }
+            bis.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return charset;
+    }
+
+
     class MyComparator implements Comparator<LyricBean> {
+
 
 
         @Override
@@ -166,7 +234,7 @@ public class LyricParaser {
             //1.根据":"切成02和04.12
             String[] s1 = timeStr.split(":");
             //2.根据“.”把04.12切成04和12
-            String[] s2 = s1[1].split("\\.");
+            String[] s2 = s1[1].split("//.");
             //3.转换成long类型的毫秒时间
             //分
             long min = Long.valueOf(s1[0]);//02
@@ -192,8 +260,8 @@ public class LyricParaser {
      */
     private int getCountTag(String line) {
         int count = 1;
-        String[] left = line.split("\\[");
-        String[] right = line.split("\\]");
+        String[] left = line.split("//[");
+        String[] right = line.split("//]");
 
         if (left.length == 0 && right.length == 0) {
             count = 1;
